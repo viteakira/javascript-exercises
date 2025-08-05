d585c230e1d7e55b54dd197b8bab37c7781d7bdc
viteakira <viteakira@users.noreply.github.com>
2025-08-05 18:55:06 +0300

    did some exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(...numbers) {
 
+    for (let i = 0; i < numbers.length; i++) {
+        if (!Number.isInteger(numbers[i])) {
+            return "ERROR";
+        }
+        if (numbers[i] < 0) {
+            return "ERROR";
+        }
+    }
+
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) { 
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+    degree = (degree - 32) * (5/9);
+    degree = Math.round(degree * 10) / 10;
+    return degree;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+    degree = ((degree * 9) / 5) + 32;
+    degree = Math.round(degree * 10) / 10;
+    return degree;
 };
 
 // Do not edit below this line
