d9ea1d0e297028c7a6e296856f74166d72abe8d2
viteakira <viteakira@users.noreply.github.com>
2025-08-05 16:39:41 +0300

    solved some exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(...parameters) {
+
+    let array = parameters[0];
+
+    for (let i = 1; i < parameters.length; i++) {
+        
+        let val = parameters[i];
+
+        while (array.includes(val)) {
+            let index = array.indexOf(val);
+
+            let tmp = array.splice(index, 1);
+        }
+    }
+    return array;
+    
 };
 
 // Do not edit below this line
